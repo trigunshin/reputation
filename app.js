c23d052a4e3a865944b6ceae97e0dc3335590897
Patrick Crane <trigunshin@gmail.com>
2012-12-07 21:51:26 -0500

    removed double invocation of rabbit

--- app.js
+++ app.js
@@ -78,24 +78,6 @@ rabbitMQ.addListener('error', function(err) {
 });
 
 
-function pub_and_sub() {
-  var exchange = conn.exchange(''); // get the default exchange
-  var queue = conn.queue('queue1', {}, function() { // create a queue
-    queue.subscribe(function(msg) { // subscribe to that queue
-      console.log(msg.body); // print new messages to the console
-    });
-    // publish a message
-    exchange.publish(queue.name, {body: 'Hello CloudAMQP!'}); 
-  });
-}
-
-var rabbitUrl = process.env.CLOUDAMQP_URL || "amqp://localhost"; // default to localhost
-var conn = amqp.createConnection({url: rabbitUrl}); // create the connection
-conn.on('ready', pub_and_sub); // when connected, call "pub_and_sub"
-conn.on('error', function(err) {
-  console.log("rabbit error:"+err);
-});
-
 function sendMsg(msg) {
   console.log(msg);
   if(exchange) {
