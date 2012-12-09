b30a9df4dc3ac42886aeec0bfe8522a7b013ba9f
Patrick Crane <trigunshin@gmail.com>
2012-12-08 23:42:49 -0500

    added space for mongo/redis

--- conf/app_conf.js
+++ conf/app_conf.js
@@ -8,5 +8,17 @@ var amqpConfig = {
   defaultURL : (process.env.CLOUDAMQP_URL||"")
 };
 
+//not yet used, need to standardize auth-including urls
+var redisConfig = {
+  defaultURL : (process.env.REDISTOGO_URL || process.env.REDIS_URL || 'localhost')
+};
+
+//not yet used, need to standardize auth-including urls
+var mongoConfig = {
+  defaultURL : (process.env.MONGOLAB_URI || process.env.MONGO_HOST || "localhost")
+};
+
 exports.emailConfig = emailConfig;
 exports.amqpConfig = amqpConfig;
+exports.redisConfig = redisConfig;
+exports.mongoConfig = mongoConfig;
