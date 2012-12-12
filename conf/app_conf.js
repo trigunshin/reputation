32c3d551abe7cd4514bebc996c260d8babaa8869
Patrick Crane <trigunshin@gmail.com>
2012-12-11 22:19:26 -0500

    updated app_conf default layout.

--- conf/app_conf.js
+++ conf/app_conf.js
@@ -1,21 +1,24 @@
+var user = (process.env.EMAIL_NAME||"");
 var emailConfig = {
-   user:    (process.env.EMAIL_NAME||""),
+   user:user,
    password:(process.env.EMAIL_PASS||""),
-   host:    (process.env.EMAIL_HOST||""),
-   ssl:      true
+   host:(process.env.EMAIL_HOST||""),
+   ssl:true,
+   userNameString:"Reputation Admin <" + user + ">"
 };
+
 var amqpConfig = {
   defaultURL : (process.env.CLOUDAMQP_URL||"")
 };
 
-//not yet used, need to standardize auth-including urls
+//need to standardize auth-including urls
 var redisConfig = {
   defaultURL : (process.env.REDISTOGO_URL || process.env.REDIS_URL || 'localhost')
 };
 
 //not yet used, need to standardize auth-including urls
 var mongoConfig = {
-  defaultURL : (process.env.MONGOLAB_URI || process.env.MONGO_HOST || "localhost")
+  defaultURL : (process.env.MONGOLAB_URI || process.env.MONGO_HOST || 'localhost')
 };
 
 exports.emailConfig = emailConfig;
