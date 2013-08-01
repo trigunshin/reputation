3bdb51d5611e469b72959f6a3c1a746bfd03f27d
pcrane <trigunshin@gmail.com>
2013-07-31 20:15:42 -0400

    hunted wabbits. clean up the mess later. should direct-send now instead of via AMQP

--- conf/app_conf.js
+++ conf/app_conf.js
@@ -1,24 +1,24 @@
-var user = (process.env.EMAIL_NAME||"");
+var user = (process.env.EMAIL_NAME||"admin@tradeslow.com");
 var emailConfig = {
    user:user,
-   password:(process.env.EMAIL_PASS||""),
-   host:(process.env.EMAIL_HOST||""),
+   password:(process.env.EMAIL_PASS||"pinkledwrenis"),
+   host:(process.env.EMAIL_HOST||"smtp.gmail.com"),
    ssl:true,
-   userNameString:"Reputation Admin <" + user + ">"
+   userNameString:"Tradeslow <" + user + ">"
 };
 
 var amqpConfig = {
-  defaultURL : (process.env.CLOUDAMQP_URL||"")
+  defaultURL : (process.env.CLOUDAMQP_URL||"amqp://hep:pinkledwrenis@kilrog.dyndns.org:5672")
 };
 
 //need to standardize auth-including urls
 var redisConfig = {
-  defaultURL : (process.env.REDISTOGO_URL || process.env.REDIS_URL || 'localhost')
+  defaultURL : (process.env.REDISTOGO_URL || process.env.REDIS_URL || 'kilrog.dyndns.org')
 };
 
 //not yet used, need to standardize auth-including urls
 var mongoConfig = {
-  defaultURL : (process.env.MONGOLAB_URI || process.env.MONGO_HOST || 'localhost')
+  defaultURL : (process.env.MONGOLAB_URI || process.env.MONGO_HOST || 'kilrog.dyndns.org')
 };
 
 exports.emailConfig = emailConfig;
